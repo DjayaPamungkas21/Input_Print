35fc76a1b758638774bed3458eec75c4b7bb035a
DjayaPamungkas21 <72689440+DjayaPamungkas21@users.noreply.github.com>
2020-10-27 10:44:04 +0700

    Add files via upload
    Python_Input&Print

---
+++ lab1_Print&Input.py
@@ -0,0 +1,25 @@
+# SOAL 1 - Menghitung rata-rata
+# Tuliskan program untuk Soal 1 di bawah ini
+n = 3
+a = eval(input("masukan angka pertama:"))
+b = eval(input("masukan angka kedua:"))
+c = eval(input("masukan angka ketiga:"))
+print ("hasil nilai rata-rata nya adalah:", (a + b + c) / n)
+print()
+# SOAL 2 - Menulis kelipatan bilangan
+# Tuliskan program untuk Soal 2 di bawah ini
+num = int(input("masukan angka bilangan bulat 5:"))
+a = print ("Kelipatan 1 :", num * 1)
+b = print ("Kelipatan 2 :", num * 2)
+c = print ("Kelipatan 3 :", num * 3)
+d = print ("Kelipatan 4 :", num * 4)
+e = print ("Kelipatan 5 :", num * 5)
+print ("kelipatanya adalah: 5---10---15---20---25")
+print()
+nom = int(input("masukan angka bilangan bulat 10:"))
+f = print ("Kelipatan 1 :", nom * 1)
+g = print ("Kelipatan 2 :", nom * 2)
+h = print ("Kelipatan 3 :", nom * 3)
+i = print ("Kelipatan 4 :", nom * 4)
+j = print ("Kelipatan 5 :", nom * 5)
+print ("kelipatanya adalah: 10---20---30---40---50")
